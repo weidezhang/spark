8cf6073aa1f570dc88647b4dd7acdf3315684cf7
Alexander Ulanov <nashb@yandex.ru>
2014-08-20 18:54:46 +0400

    mllib Updater and Gradient interfaces implemented

--- mllib/src/test/scala/org/apache/spark/mllib/classification/NeuralNetworksSuite.scala
+++ mllib/src/test/scala/org/apache/spark/mllib/classification/NeuralNetworksSuite.scala
@@ -1,25 +1,39 @@
 package org.apache.spark.mllib.classification
 
+import org.apache.spark.SparkContext
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
 import org.apache.spark.mllib.linalg.Vectors
-import org.apache.spark.mllib.util.LocalSparkContext
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.util.{MLUtils, LocalSparkContext}
 import org.scalatest.FunSuite
 import scala.math.random
 
 class NeuralNetworksSuite extends FunSuite with LocalSparkContext{
 
+  /* training set */
+  val inputs = Array[Array[Double]](
+    Array[Double](0,0),
+    Array[Double](0,1),
+    Array[Double](1,0),
+    Array[Double](1,1)
+  )
+  val outputs = Array[Double](0, 1, 1, 0)
+  /* NN */
+  val inputSize = 2
+  val hiddenSize = 5
+  val outputSize = 1
+
+
+  test("model") {
+    val data = inputs.zip(outputs).map{ case(features, label) =>
+      new LabeledPoint(label, Vectors.dense(features))}
+    val rddData = sc.parallelize(data, 2)
+    val predictor = NeuralNetwork.train(rddData, Array(hiddenSize))
+    val result = rddData.map(lp => (predictor.predict(lp.features), lp.label))
+    result.foreach(x => println(x._1 +":" + x._2))
+  }
+
   test("xor") {
-    /* training set */
-    val inputs = Array[Array[Double]](
-      Array[Double](0,0),
-      Array[Double](0,1),
-      Array[Double](1,0),
-      Array[Double](1,1)
-    )
-    val outputs = Array[Double](0, 1, 1, 0)
-    /* NN */
-    val inputSize = 2
-    val hiddenSize = 5
-    val outputSize = 1
     val sizes = Array(inputSize, hiddenSize, outputSize)
     var size = 0
     for(i <- 1 until sizes.size){
@@ -28,7 +42,7 @@ class NeuralNetworksSuite extends FunSuite with LocalSparkContext{
     val nn = new NeuralNetworkGradient(sizes)
     /* training */
     var weights = Array.fill(size){random * (2.4 *2)- 2.4}
-    for(i <- 0 until 1000){
+    for(i <- 0 until 100){
       for(j <-0 until inputs.size){
         val (gradient, loss) = nn.compute(Vectors.dense(inputs(j)), outputs(j), Vectors.dense(weights))
         weights = weights.zip(gradient.toArray).map{ case(x, y) => x + y }
@@ -40,7 +54,25 @@ class NeuralNetworksSuite extends FunSuite with LocalSparkContext{
       println(model.predict(Vectors.dense(inputs(j))))
     }
 
+    println(Vectors.dense(weights))
 
   }
+
+ test("real data") {
+    val data = MLUtils.loadLibSVMFile(sc, "c:/ulanov/res/sentiment/thumbsup/films100.libsvm")
+    val split = data.randomSplit(Array(0.9, 0.1), 11L)
+    val training = split(0)
+    val test = split(1)
+
+   val predictor = NeuralNetwork.train(training, Array(60), 300)
+   val predictionAndLabels = test.map { point =>
+     val score = predictor.predict(point.features)
+     (score, point.label)
+   }
+   val metrics = new MulticlassMetrics(predictionAndLabels)
+   metrics.labels.foreach( l => println(metrics.fMeasure(l)))
+
+  }
+
 
 }
