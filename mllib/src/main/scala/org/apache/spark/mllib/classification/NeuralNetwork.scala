39f7eb7ef1c7cf2f87935aa1caafe081d6daa674
Alexander Ulanov <nashb@yandex.ru>
2014-08-21 14:25:29 +0400

    Documentation

--- mllib/src/main/scala/org/apache/spark/mllib/classification/NeuralNetwork.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/classification/NeuralNetwork.scala
@@ -17,25 +17,31 @@
 
 package org.apache.spark.mllib.classification
 
+import scala.math._
+import breeze.linalg.{axpy => brzAxpy, Vector => BV}
 import org.apache.spark.mllib.linalg
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.optimization.{Updater, GradientDescent, Gradient}
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.rdd.RDD
 import org.apache.spark.mllib.linalg.BLAS.{axpy}
-import breeze.linalg.{axpy => brzAxpy, Vector => BV}
+import org.apache.spark.annotation.Experimental
 
-import scala.math._
-
-private[classification] trait NNetworkHelper {
+/**
+ * ::Experimental::
+ * Helper Trait for neural network
+ * Implements addressing to array of weights
+ */
+@Experimental
+private[classification] trait NeuralNetworkHelper {
 
   def layers: Array[Int]
   def sigmoid ( value: Double) : Double = 1d / (1d + exp(-value))
-  lazy val weightCount = (for(i <- 1 until layers.size) yield layers(i - 1) * layers(i)).sum
-  lazy val outputCount = layers.sum - (if (layers.size > 0) layers(0) else 0)
-  //def outputs: Array[Double]
+  protected lazy val weightCount =
+    (for(i <- 1 until layers.size) yield layers(i - 1) * layers(i)).sum
+  protected lazy val outputCount = layers.sum - (if (layers.size > 0) layers(0) else 0)
 
-  def weightIndex(layer: Int, neuron: Int, connection: Int): Int = {
+  protected def weightIndex(layer: Int, neuron: Int, connection: Int): Int = {
     var layerOffset = 0
     if(layer == 0) {
       layerOffset = 0
@@ -47,7 +53,7 @@ private[classification] trait NNetworkHelper {
     layerOffset + neuron * layers(layer - 1) + connection
   }
 
-  def outputIndex(layer: Int, neuron: Int): Int = {
+  protected def outputIndex(layer: Int, neuron: Int): Int = {
     var layerOffset = 0
     for (i <- 1 until layer) {
       layerOffset += layers(i)
@@ -55,28 +61,51 @@ private[classification] trait NNetworkHelper {
     layerOffset + neuron
   }
 
-  def forwardRun(weights: Array[Double], inputs: Array[Double]) = {
+  protected def forwardRun(weights: Array[Double], inputs: Array[Double]) = {
     /* TODO implement forward run and share across gradient and model */
   }
 }
 
+/**
+ * ::Experimental::
+ * Trait for label conversion
+ * for Neural Network
+ */
+@Experimental
 private[classification] trait LabelConverter {
 
+  /**
+   * Returns the number of network ouputs
+   * (of the outer layer)
+   */
   protected def resultCount: Int
 
+  /**
+   * Returns an array of double that has 1.0
+   * at the index equals to label.toInt and
+   * other element are zero
+   * When resultCount is 2 then
+   * returns array of size 1 with label
+   * @param label label
+   */
   protected def label2Array(label: Double): Array[Double] = {
     val result = Array.fill(resultCount)(0.0)
     if (resultCount == 1) result(0) = label else result(label.toInt) = 1.0
     result
   }
 
+  /**
+   * Returns a label that corresponds to the
+   * array of double
+   * @param resultArray array that represents a label for neural network
+   */
   protected def array2Label(resultArray: Array[Int]): Double = {
     if(resultArray.size == 1) resultArray(0) else resultArray.indexOf(1.0).toDouble
   }
 }
 
 private class NeuralNetworkGradient(layerSizes: Array[Int])
-  extends Gradient with NNetworkHelper with LabelConverter{
+  extends Gradient with NeuralNetworkHelper with LabelConverter{
 
   override def layers = layerSizes
   override def resultCount = layers.last
@@ -139,7 +168,6 @@ private class NeuralNetworkGradient(layerSizes: Array[Int])
       }
       layerIndex -= 1
     }
-
     /* perceptron weights gradient computation */
     layerIndex = layers.size - 1
     while (layerIndex > 0) {
@@ -183,13 +211,26 @@ private class NeuralNetworkGradient(layerSizes: Array[Int])
 
 }
 
+/**
+ * ::Experimental::
+ * Neural network model.
+ *
+ * @param layerArray array of layer sizes,
+ * first is the input size of the network,
+ * last is the output size of the network.
+ * @param weights vector of weights of
+ * neurons inputs, should have the size
+ * input*hidden(0) + hidden(0)*hidden(1)
+ * + ... + hidden(N)*output
+ */
+@Experimental
 class NeuralNetworkModel(layerArray: Array[Int], val weights: linalg.Vector)
-  extends ClassificationModel with NNetworkHelper with LabelConverter with Serializable {
+  extends ClassificationModel with NeuralNetworkHelper with LabelConverter with Serializable {
 
   override def layers = layerArray
   override def resultCount = layers.last
   require(weightCount == weights.size)
-  val outputs = Array.fill(outputCount){0.0}
+  private val outputs = Array.fill(outputCount){0.0}
 
   override def predict(testData: RDD[linalg.Vector]): RDD[Double] = {
     testData.map(predict(_))
@@ -231,6 +272,12 @@ class NeuralNetworkModel(layerArray: Array[Int], val weights: linalg.Vector)
   }
 }
 
+/**
+ * ::Experimental::
+ * Trains Neural network classifier
+ * NOTE: labels should represent the class index
+ */
+@Experimental
 class NeuralNetwork private (hiddenLayers: Array[Int], numIterations: Int, learningRate: Double)
   extends Serializable {
 
@@ -260,18 +307,45 @@ private class GradientUpdater extends Updater {
   }
 }
 
+/**
+ * ::Experimental::
+ * Fabric for Neural Network classifier
+ */
+@Experimental
 object NeuralNetwork {
 
+  /**
+   * Trains a Neural Network model given an RDD of `(label, features)` pairs.
+   *
+   * @param data RDD of `(label, array of features)` pairs.
+   * @param hiddenLayers array of hidden layers sizes
+   * @param numIterations number of iterations
+   * @param learningRate learning rate
+   */
   def train(data: RDD[LabeledPoint], hiddenLayers: Array[Int],
             numIterations: Int, learningRate: Double) : NeuralNetworkModel = {
     val nn = new NeuralNetwork(hiddenLayers, numIterations, learningRate)
     nn.run(data)
   }
 
+  /**
+   * Trains a Neural Network model given an RDD of `(label, features)` pairs.
+   * Does 1000 iterations with learning rate 0.9
+   *
+   * @param data RDD of `(label, array of features)` pairs.
+   * @param hiddenLayers array of hidden layers sizes
+   */
   def train(data: RDD[LabeledPoint], hiddenLayers: Array[Int]) : NeuralNetworkModel = {
     train(data, hiddenLayers, 1000, 0.9)
   }
 
+  /**
+   * Trains a Neural Network model given an RDD of `(label, features)` pairs.
+   * Creates one hidden layer of size = (input + ouput) / 2
+   * Does 1000 iterations with learning rate 0.9
+   *
+   * @param data RDD of `(label, array of features)` pairs.
+   */
   def train(data: RDD[LabeledPoint]) : NeuralNetworkModel = {
     train(data, Array(0), 1000, 0.9)
   }
