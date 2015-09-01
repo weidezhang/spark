849480235507f139f206064d42b8bc3581712e4a
Alexander Ulanov <nashb@yandex.ru>
2015-09-01 16:21:35 -0700

    Autoencoder draft

---
+++ mllib/src/main/scala/org/apache/spark/ml/feature/Autoencoder.scala
@@ -0,0 +1,158 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.ml.feature
+
+import org.apache.spark.annotation.Experimental
+import org.apache.spark.ml.ann.{FeedForwardTrainer, FeedForwardTopology}
+import org.apache.spark.ml.classification.MultilayerPerceptronParams
+import org.apache.spark.ml.param.shared.{HasInputCol, HasOutputCol}
+import org.apache.spark.ml.util.Identifiable
+import org.apache.spark.ml.{Model, Estimator}
+import org.apache.spark.ml.param.{IntParam, Params, ParamMap}
+import org.apache.spark.mllib.linalg.{VectorUDT, Vectors, Vector}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{Row, DataFrame}
+import org.apache.spark.sql.types.{StructField, StructType}
+
+/**
+ * Params for [[Autoencoder]] and [[AutoencoderModel]].
+ */
+private[feature] trait AutoencoderParams extends Params with HasInputCol with HasOutputCol {
+}
+
+/**
+ * :: Experimental ::
+ * Autoencoder.
+ */
+@Experimental
+class Autoencoder (override val uid: String) extends Estimator[AutoencoderModel]
+  with MultilayerPerceptronParams with AutoencoderParams  {
+
+  def this() = this(Identifiable.randomUID("autoencoder"))
+
+  // TODO: make sure that user understands how to set it
+  /** @group setParam */
+  def setLayers(value: Array[Int]): this.type = set(layers, value)
+
+  /** @group setParam */
+  def setBlockSize(value: Int): this.type = set(blockSize, value)
+
+  /**
+   * Set the maximum number of iterations.
+   * Default is 100.
+   * @group setParam
+   */
+  def setMaxIter(value: Int): this.type = set(maxIter, value)
+
+  /**
+   * Set the convergence tolerance of iterations.
+   * Smaller value will lead to higher accuracy with the cost of more iterations.
+   * Default is 1E-4.
+   * @group setParam
+   */
+  def setTol(value: Double): this.type = set(tol, value)
+
+  /**
+   * Set the seed for weights initialization.
+   * @group setParam
+   */
+  def setSeed(value: Long): this.type = set(seed, value)
+
+  /**
+   * Fits a model to the input data.
+   */
+  override def fit(dataset: DataFrame): AutoencoderModel = {
+    val data = dataset.select($(inputCol)).map { case Row(x: Vector) => (x, x) }
+    val myLayers = $(layers)
+    // TODO: initialize topology based on the data type (binary, real [0..1], real)
+    // binary => false + cross entropy
+    // real [0..1] => false + sq error
+    // real [0..1] that sum to one => true + cross entropy
+    // real => remove the top layer + sq error
+    val topology = FeedForwardTopology.multiLayerPerceptron(myLayers, false)
+    val FeedForwardTrainer = new FeedForwardTrainer(topology, myLayers(0), myLayers.last)
+    FeedForwardTrainer.LBFGSOptimizer.setConvergenceTol($(tol)).setNumIterations($(maxIter))
+    FeedForwardTrainer.setStackSize($(blockSize))
+    val autoencoderModel = FeedForwardTrainer.train(data)
+    // TODO: send the real params of the network since it might not be a simple MLP
+    new AutoencoderModel(uid, myLayers, autoencoderModel.weights())
+  }
+
+  override def copy(extra: ParamMap): Estimator[AutoencoderModel] = defaultCopy(extra)
+
+  /**
+   * :: DeveloperApi ::
+   *
+   * Derives the output schema from the input schema.
+   */
+  override def transformSchema(schema: StructType): StructType = {
+    val inputType = schema($(inputCol)).dataType
+    require(inputType.isInstanceOf[VectorUDT],
+      s"Input column ${$(inputCol)} must be a vector column")
+    require(!schema.fieldNames.contains($(outputCol)),
+      s"Output column ${$(outputCol)} already exists.")
+    val outputFields = schema.fields :+ StructField($(outputCol), new VectorUDT, false)
+    StructType(outputFields)
+  }
+}
+/**
+ * :: Experimental ::
+ * Autoencoder model.
+ *
+ * @param layers array of layer sizes including input and output
+ * @param weights weights (or parameters) of the model
+ */
+@Experimental
+class AutoencoderModel private[ml] (override val uid: String,
+                                    layers: Array[Int],
+                                    weights: Vector)
+  extends Model[AutoencoderModel] with AutoencoderParams {
+
+  // TODO: make sure that the same topology is created as in Autoencoder
+  private val autoecoderModel =
+    FeedForwardTopology.multiLayerPerceptron(layers, true).getInstance(weights)
+
+
+  override def copy(extra: ParamMap): AutoencoderModel = {
+    copyValues(new AutoencoderModel(uid, layers, weights), extra)
+  }
+
+  /**
+   * Transforms the input dataset.
+   */
+  override def transform(dataset: DataFrame): DataFrame = {
+    transformSchema(dataset.schema, logging = true)
+    val pcaOp = udf { autoecoderModel.predict _ }
+    dataset.withColumn($(outputCol), pcaOp(col($(inputCol))))
+  }
+
+  /**
+   * :: DeveloperApi ::
+   *
+   * Derives the output schema from the input schema.
+   */
+  override def transformSchema(schema: StructType): StructType = {
+    val inputType = schema($(inputCol)).dataType
+    require(inputType.isInstanceOf[VectorUDT],
+      s"Input column ${$(inputCol)} must be a vector column")
+    require(!schema.fieldNames.contains($(outputCol)),
+      s"Output column ${$(outputCol)} already exists.")
+    val outputFields = schema.fields :+ StructField($(outputCol), new VectorUDT, false)
+    StructType(outputFields)
+  }
+}
